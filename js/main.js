6ebcfc60ed6b94ccbfc8762b367f5ada4919aa0a
Walentein <vlad_bars@ukr.net>
2021-08-05 13:54:02 +0300

    prettier reconfigured

--- js/main.js
+++ js/main.js
@@ -24,18 +24,18 @@ const getTaskText = (element) => getTaskElem(element).querySelector(".task");
 const stringLastWord = (text) => text.split(" ").pop();
 
 const refreshCheckboxAndRemovesLists = function () {
-  checkboxes = document.querySelectorAll("input[name='task-status']");
-  removes = document.querySelectorAll("[title='Remove item']");
-  restartEventListeners();
+    checkboxes = document.querySelectorAll("input[name='task-status']");
+    removes = document.querySelectorAll("[title='Remove item']");
+    restartEventListeners();
 };
 
 const calculateCompletedTasks = function () {
-  let counter = 0;
-  for (let i = 10000; ; i++) {
-    if (getItem(i)) counter++;
-    else break;
-  }
-  return counter;
+    let counter = 0;
+    for (let i = 10000; ; i++) {
+        if (getItem(i)) counter++;
+        else break;
+    }
+    return counter;
 };
 
 //calculated variables
@@ -45,91 +45,88 @@ let insertPosition = completedTasksNumber + 10000;
 //========================================================
 
 const recalcTaskVariables = function () {
-  totalTasksNumber = Object.keys(localStorage).length;
-  completedTasksNumber = calculateCompletedTasks();
-  tasksNumber = totalTasksNumber - completedTasksNumber;
-  insertPosition = completedTasksNumber + 10000;
+    totalTasksNumber = Object.keys(localStorage).length;
+    completedTasksNumber = calculateCompletedTasks();
+    tasksNumber = totalTasksNumber - completedTasksNumber;
+    insertPosition = completedTasksNumber + 10000;
 };
 
 const saveToStorage = function () {
-  let taskValue;
-  for (let i = 0; i < checkboxes.length; i++) {
-    taskValue = getTaskText(checkboxes[i]).innerHTML;
-    if (checkboxes[i].checked) {
-      taskValue += checkedMark;
-      saveCompletedTask(taskValue);
+    let taskValue;
+    for (let i = 0; i < checkboxes.length; i++) {
+        taskValue = getTaskText(checkboxes[i]).innerHTML;
+        if (checkboxes[i].checked) {
+            taskValue += checkedMark;
+            saveCompletedTask(taskValue);
+        }
+        saveItem(i, taskValue);
     }
-    saveItem(i, taskValue);
-  }
 };
 
 const saveCompletedTask = function (item) {
-  recalcTaskVariables();
-  let flagInsert = true;
-  let taskText;
-  for (let i = 10000; i < insertPosition + 1; i++) {
-    taskText = item;
-    flagInsert = taskText === getItem(i) ? false : true;
-    if (flagInsert === false) break;
-  }
-  if (flagInsert) saveItem(insertPosition, taskText);
+    recalcTaskVariables();
+    let flagInsert = true;
+    let taskText;
+    for (let i = 10000; i < insertPosition + 1; i++) {
+        taskText = item;
+        flagInsert = taskText === getItem(i) ? false : true;
+        if (flagInsert === false) break;
+    }
+    if (flagInsert) saveItem(insertPosition, taskText);
 };
 
 const reassignStorageKeys = function (startingKey, flagCompletedTasks) {
-  let count;
-  if (!flagCompletedTasks) count = tasksNumber;
-  else count = insertPosition;
-  for (let i = startingKey + 1; i < count; i++) {
-    if (getItem(i)) {
-      saveItem(i - 1, getItem(i));
+    let count;
+    if (!flagCompletedTasks) count = tasksNumber;
+    else count = insertPosition;
+    for (let i = startingKey + 1; i < count; i++) {
+        if (getItem(i)) {
+            saveItem(i - 1, getItem(i));
+        }
     }
-  }
-  removeItem(count - 1);
+    removeItem(count - 1);
 };
 
 const removeCompletedTask = function (item) {
-  let taskText = item + checkedMark;
-  for (let i = 10000; i < insertPosition + 1; i++) {
-    if (taskText === getItem(i)) {
-      removeItem(i);
-      reassignStorageKeys(i, true);
+    let taskText = item + checkedMark;
+    for (let i = 10000; i < insertPosition + 1; i++) {
+        if (taskText === getItem(i)) {
+            removeItem(i);
+            reassignStorageKeys(i, true);
+        }
     }
-  }
 };
 
 const removeFromStorage = function (element) {
-  recalcTaskVariables();
-  let taskValue = getRemoveElem(element).querySelector(".task").innerHTML;
-  let modifiedTaskValue = taskValue + checkedMark;
-  for (let i = 0; i < removes.length; i++) {
-    if (getItem(i) === taskValue || getItem(i) === modifiedTaskValue) {
-      removeItem(i);
-      reassignStorageKeys(i, false);
+    recalcTaskVariables();
+    let taskValue = getRemoveElem(element).querySelector(".task").innerHTML;
+    let modifiedTaskValue = taskValue + checkedMark;
+    for (let i = 0; i < removes.length; i++) {
+        if (getItem(i) === taskValue || getItem(i) === modifiedTaskValue) {
+            removeItem(i);
+            reassignStorageKeys(i, false);
+        }
     }
-  }
 };
 
 const restoreFromStorage = function () {
-  let taskValue;
-  for (let i = 0; i < tasksNumber; i++) {
-    taskValue = getItem(i);
-    if (modifiedCheckedMark === stringLastWord(taskValue)) {
-      let modifiedTaskValue = taskValue.substring(
-        0,
-        taskValue.lastIndexOf(" ")
-      );
-      insertTask(modifiedTaskValue, true);
-    } else {
-      insertTask(taskValue, false);
+    let taskValue;
+    for (let i = 0; i < tasksNumber; i++) {
+        taskValue = getItem(i);
+        if (modifiedCheckedMark === stringLastWord(taskValue)) {
+            let modifiedTaskValue = taskValue.substring(0, taskValue.lastIndexOf(" "));
+            insertTask(modifiedTaskValue, true);
+        } else {
+            insertTask(taskValue, false);
+        }
     }
-  }
 };
 
 const insertTask = function (task, checked) {
-  let taskList = document.querySelector("#task-list");
-  let taskCode;
-  if (checked) {
-    taskCode = `<li id="list-item" class="list-group-item d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
+    let taskList = document.querySelector("#task-list");
+    let taskCode;
+    if (checked) {
+        taskCode = `<li id="list-item" class="list-group-item d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
             <div id="input-task" class="d-flex align-items-center">
                 <input name="task-status" class="form-check-input me-2" type="checkbox" value="" aria-label="..." checked />
                 <span class="task task--ready">${task}</span>
@@ -138,8 +135,8 @@ const insertTask = function (task, checked) {
                 <i class="fas fa-times text-primary"></i>
             </a>
         </li>`;
-  } else {
-    taskCode = `<li id="list-item" class="list-group-item d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
+    } else {
+        taskCode = `<li id="list-item" class="list-group-item d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
             <div id="input-task" class="d-flex align-items-center">
                 <input name="task-status" class="form-check-input me-2" type="checkbox" value="" aria-label="..." />
                 <span class="task">${task}</span>
@@ -148,69 +145,69 @@ const insertTask = function (task, checked) {
                 <i class="fas fa-times text-primary"></i>
             </a>
         </li>`;
-  }
-  taskList.insertAdjacentHTML("beforeend", taskCode);
-  refreshCheckboxAndRemovesLists();
-  restartEventListeners();
-  saveToStorage();
-  return document.createTextNode(taskCode);
+    }
+    taskList.insertAdjacentHTML("beforeend", taskCode);
+    refreshCheckboxAndRemovesLists();
+    restartEventListeners();
+    saveToStorage();
+    return document.createTextNode(taskCode);
 };
 
 const startEventListeners = function () {
-  for (let checkbox of checkboxes) {
-    checkbox.addEventListener("change", function () {
-      let taskText;
-      if (this.checked) {
-        taskText = getTaskText(this);
-        if (!taskText.classList.contains("task--ready")) {
-          taskText.classList.add("task--ready");
-          saveToStorage();
-        }
-      } else {
-        taskText = getTaskText(this);
-        if (taskText.classList.contains("task--ready")) {
-          taskText.classList.remove("task--ready");
-          saveToStorage();
-        }
-        removeCompletedTask(taskText.innerHTML);
-      }
-    });
-  }
+    for (let checkbox of checkboxes) {
+        checkbox.addEventListener("change", function () {
+            let taskText;
+            if (this.checked) {
+                taskText = getTaskText(this);
+                if (!taskText.classList.contains("task--ready")) {
+                    taskText.classList.add("task--ready");
+                    saveToStorage();
+                }
+            } else {
+                taskText = getTaskText(this);
+                if (taskText.classList.contains("task--ready")) {
+                    taskText.classList.remove("task--ready");
+                    saveToStorage();
+                }
+                removeCompletedTask(taskText.innerHTML);
+            }
+        });
+    }
 
-  for (let remove of removes) {
-    remove.addEventListener("click", function () {
-      let task = getRemoveElem(this);
-      task.remove();
-      removeFromStorage(this);
-      refreshCheckboxAndRemovesLists();
-    });
-  }
+    for (let remove of removes) {
+        remove.addEventListener("click", function () {
+            let task = getRemoveElem(this);
+            task.remove();
+            removeFromStorage(this);
+            refreshCheckboxAndRemovesLists();
+        });
+    }
 };
 
 const stopEventListeners = function () {
-  for (let checkbox of checkboxes) {
-    checkbox.removeEventListener("change", function () {});
-  }
+    for (let checkbox of checkboxes) {
+        checkbox.removeEventListener("change", function () {});
+    }
 
-  for (let remove of removes) {
-    remove.removeEventListener("click", function () {});
-  }
+    for (let remove of removes) {
+        remove.removeEventListener("click", function () {});
+    }
 };
 
 const restartEventListeners = function () {
-  stopEventListeners();
-  startEventListeners();
+    stopEventListeners();
+    startEventListeners();
 };
 
 //code
 document.addEventListener("DOMContentLoaded", function () {
-  restoreFromStorage();
+    restoreFromStorage();
 });
 
 startEventListeners();
 
 submitButton.addEventListener("click", function () {
-  let taskText = document.getElementById("form__add-task").value;
-  document.getElementById("form__add-task").value = "";
-  if (taskText) insertTask(taskText, false);
+    let taskText = document.getElementById("form__add-task").value;
+    document.getElementById("form__add-task").value = "";
+    if (taskText) insertTask(taskText, false);
 });
