015d61df9db5ce70fb51c6ef871cc898703261eb
Walentein <vlad_bars@ukr.net>
2021-08-12 22:36:57 +0300

    removed node_modules due to more efficient solution

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 "use strict";
 
-import { v4 as uuidv4 } from "uuid";
-
 //setting system getters
 const querySelect = (item) => document.querySelector(item);
 const querySelectAll = (item) => document.querySelectorAll(item);
