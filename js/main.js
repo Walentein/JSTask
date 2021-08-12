015f4712945da78a2a50901a368cb27d1a275d1f
Walentein <vlad_bars@ukr.net>
2021-08-12 21:42:32 +0300

    installed node modules + uuid

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 "use strict";
+
+import { v4 as uuidv4 } from "uuid";
+
 //setting system getters
 const querySelect = (item) => document.querySelector(item);
 const querySelectAll = (item) => document.querySelectorAll(item);
