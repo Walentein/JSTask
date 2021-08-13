fbebde05eb6963cc3a74f22c6309e89abf2f2b50
Walentein <vlad_bars@ukr.net>
2021-08-13 10:45:37 +0300

    added FindTask method reworked eventListeners small imporvments

--- js/main.js
+++ js/main.js
@@ -9,11 +9,6 @@ const getByID = (item) => document.getElementById(item);
 const listStorage = window.localStorage;
 let totalTasksNumber = Object.keys(localStorage).length;
 
-const submitButton = getByID("add-task-button");
-const completeButton = getByID("show-completed-button");
-let checkboxes = querySelectAll("input[name='task-status']");
-let removes = querySelectAll("[title='Remove item']");
-
 //declaring functions
 const createTask = ({ id, checked, taskValue }) => (task = { id: id, checked: checked, taskValue: taskValue });
 
@@ -25,26 +20,23 @@ const clearListStorage = () => listStorage.clear();
 const prepareForStaorage = (item) => JSON.stringify(item);
 const retrieveFromStorage = (item) => JSON.parse(item);
 
+const findTaskInStorage = (task) => {
+    Object.keys(localStorage).forEach(function (key) {
+        taskObj = retrieveFromStorage(getItem(key));
+        if (taskObj.taskValue === taskText) {
+            return task.id;
+        } else {
+            return null;
+        }
+    });
+};
+
 const getRemoveElem = (element) => element.parentNode;
 const getTaskElem = (element) => element.parentNode.parentNode;
 const getTaskText = (element) => getTaskElem(element).querySelector(".task");
 const getCompletedTaskList = (element) => getTaskElem(element).parentNode;
 
 const generateID = () => Date.now();
-
-const saveToStorage = () => {
-    let taskValue;
-    let id;
-    let checked;
-    for (let checkbox of checkboxes) {
-        taskValue = getTaskText(checkbox).innerHTML;
-        id = generateID();
-        checked = checkbox.checked ? true : false;
-        taskObj = createTask({ id, checked, taskValue });
-        task = prepareForStaorage(taskObj);
-        saveItem(taskObj.id, task);
-    }
-};
 
 const removeFromStorage = (item) => {
     let taskObj;
@@ -86,11 +78,11 @@ const taskExists = (task) => {
 const renderTask = (task) => {
     let taskCode = `<li id="list-item" class="list-group-item d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
                 <div id="input-task" class="d-flex align-items-center">
-                    <input name="task-status" class="form-check-input me-2" type="checkbox" value="" aria-label="..." />
+                    <input id="task-status" name="task-status" class="form-check-input me-2" type="checkbox" value="" aria-label="..." />
                     <span class="task">${task}</span>
                 </div>
                     <a href="#!" data-mdb-toggle="tooltip" title="Remove item">
-                    <i class="fas fa-times text-primary"></i>
+                    <i id="remove-item" class="fas fa-times text-primary"></i>
                 </a>
             </li>`;
     return taskCode;
@@ -116,49 +108,50 @@ const insertTask = (task) => {
         taskCode = renderCompletedTask(task.taskValue);
     }
     taskList.insertAdjacentHTML("afterbegin", taskCode);
-    saveToStorage();
     return document.createTextNode(taskCode);
 };
 
 //code
-getByID("task-list").addEventListener("change", function (e) {
-    if (e.target && e.target.tagName === "INPUT") {
-        if (e.target.checked) {
-            let taskText = getTaskText(e.target);
-            taskText.classList.add("task--ready");
-            insertTask({ checked: true, taskValue: taskText.innerHTML });
-            saveToStorage();
-            getTaskElem(taskText).remove();
-        }
-    }
-});
 
-getByID("task-list").addEventListener("click", function (e) {
-    if (e.target && e.target.tagName === "I") {
-        let task = getTaskElem(e.target);
-        task.remove();
-        removeFromStorage(e.target.parentNode);
-    }
-});
-
+/*window.addEventListener("onload", () => {
+    restoreFromStorage();
+});*/
 document.addEventListener("DOMContentLoaded", function () {
     restoreFromStorage();
 });
 
-submitButton.addEventListener("click", function (event) {
+document.addEventListener("click", (event) => {
     event.preventDefault();
-    let taskText = getByID("form__add-task").value;
-    getByID("form__add-task").value = "";
-    if (taskText.trim().length !== 0 && taskExists(taskText)) {
-        insertTask({ taskValue: taskText, checked: false });
-    }
-});
-
-completeButton.addEventListener("click", function () {
-    let completedList = querySelect("#done-list-container");
-    if (completedList.classList.contains("hidden")) {
-        completedList.classList.remove("hidden");
-    } else {
-        completedList.classList.add("hidden");
+    let elementId = event.target.id;
+    console.log(event.target.id);
+    switch (elementId) {
+        case "remove-item":
+            let task = getTaskElem(event.target);
+            task.remove();
+            removeFromStorage(event.target.parentNode);
+            break;
+        case "task-status":
+            if (event.target.checked) {
+                let taskText = getTaskText(event.target);
+                taskText.classList.add("task--ready");
+                insertTask({ checked: true, taskValue: taskText.innerHTML });
+                getTaskElem(taskText).remove();
+            }
+            break;
+        case "show-completed-button":
+            let completedList = querySelect("#done-list-container");
+            if (completedList.classList.contains("hidden")) {
+                completedList.classList.remove("hidden");
+            } else {
+                completedList.classList.add("hidden");
+            }
+            break;
+        case "add-task-button":
+            let taskText = getByID("form__add-task").value;
+            getByID("form__add-task").value = "";
+            if (taskText.trim().length !== 0 && taskExists(taskText)) {
+                insertTask({ taskValue: taskText, checked: false });
+            }
+            break;
     }
 });
